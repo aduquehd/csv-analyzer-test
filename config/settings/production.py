a038f63bd7b24e750b15a28705d22b0637182cc2
saduqz <saduqz@gmail.com>
2020-07-20 22:55:33 -0500

    Added flower to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 from sentry_sdk.integrations.celery import CeleryIntegration
 
-
 from .base import *  # noqa
 from .base import env
 
@@ -14,7 +13,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["saduqz.com"])
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["saduqz.com"])
+ALLOWED_HOSTS = ["saduqz.com", "www.saduqz.com", "flower.saduqz.com"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -92,7 +92,7 @@ LOGGING = {
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
+                      "%(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
